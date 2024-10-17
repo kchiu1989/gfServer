361cbe9fd4aedf71f51566936cfbfc51d30b30be
kayda <291126509@qq.com>
2024-10-17 18:01:54 +0800

    增加业务常量

---
+++ interface-sync-service/src/main/java/com/gf/biz/bizCommon/BizCommonConstant.java
@@ -0,0 +1,31 @@
+package com.gf.biz.bizCommon;
+
+/*
+ *    Copyright (c) 2018-2025, Gf All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the XX科技 developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: Gf (1414798079@qq.com)
+ */
+
+
+/**
+ * @author Gf
+ * @date 2017/10/29
+ */
+public interface BizCommonConstant {
+
+    String PI_SCORE_EXCEPTION_REASON_1 = "未查询到数据";
+    String PI_SCORE_DIMENSION_FLAG_MONTH="0";//月度指标
+    String PI_SCORE_DIMENSION_FLAG_QUARTER="1";//季度指标
+
+}
