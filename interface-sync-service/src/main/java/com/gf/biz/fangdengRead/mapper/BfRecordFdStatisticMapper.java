717c4f719a4a48ab61fcae84a948e73c7ea794e5
kayda <291126509@qq.com>
2024-10-17 15:21:00 +0800

    增加樊登数据的处理和生单

---
+++ interface-sync-service/src/main/java/com/gf/biz/fangdengRead/mapper/BfRecordFdStatisticMapper.java
@@ -0,0 +1,16 @@
+package com.gf.biz.fangdengRead.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gf.biz.fangdengRead.entity.BfRecordFdStatistic;
+
+/**
+ * <p>
+ * 樊登人员读书统计表 Mapper 接口
+ * </p>
+ *
+ * @author Gf
+ * @since 2024-10-16 17:57:03
+ */
+    public interface BfRecordFdStatisticMapper extends BaseMapper<BfRecordFdStatistic> {
+
+}
---
+++ interface-sync-service/src/main/java/com/gf/biz/fangdengRead/mapper/IfRecordFdScoreMapper.java
@@ -0,0 +1,16 @@
+package com.gf.biz.fangdengRead.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gf.biz.fangdengRead.entity.IfRecordFdScore;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Gf
+ * @since 2024-10-16 16:36:48
+ */
+    public interface IfRecordFdScoreMapper extends BaseMapper<IfRecordFdScore> {
+
+}
