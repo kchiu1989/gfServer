a2048659268eed80b8fc2f92b6281d0eb820e83b
kayda <291126509@qq.com>
2024-10-18 13:42:33 +0800

    增加管理费用合计跑分任务

---
+++ interface-sync-service/src/main/java/com/gf/biz/funcDeptIndicatorScore/task/CalculateTotalAdmExpenseIndicatorScore.java
@@ -0,0 +1,10 @@
+package com.gf.biz.funcDeptIndicatorScore.task;
+
+import com.xxl.job.core.handler.IJobHandler;
+
+public class CalculateTotalAdmExpenseIndicatorScore extends IJobHandler {
+    @Override
+    public void execute() throws Exception {
+
+    }
+}
