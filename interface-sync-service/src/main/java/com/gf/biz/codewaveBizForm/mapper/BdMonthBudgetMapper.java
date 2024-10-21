e73c0819b5c27df8a84e59b8fe1bc41a07811cb3
kayda <291126509@qq.com>
2024-10-21 16:31:19 +0800

    增加业务通用方法

---
+++ interface-sync-service/src/main/java/com/gf/biz/codewaveBizForm/mapper/BdMonthBudgetMapper.java
@@ -0,0 +1,16 @@
+package com.gf.biz.codewaveBizForm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gf.biz.codewaveBizForm.po.BdMonthBudget;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Gf
+ * @since 2024-10-18 17:08:44
+ */
+    public interface BdMonthBudgetMapper extends BaseMapper<BdMonthBudget> {
+
+}
