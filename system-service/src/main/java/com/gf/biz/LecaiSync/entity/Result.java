658c25a96523254c075f2901acd7da66acf3001e
kayda <291126509@qq.com>
2024-10-22 17:19:37 +0800

    公共代码进行优化

--- system-service/src/main/java/com/gf/biz/LecaiSync/entity/Result.java
+++ system-service/src/main/java/com/gf/biz/LecaiSync/entity/Result.java
@@ -1,16 +1,16 @@
 package com.gf.biz.LecaiSync.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
+
 public class Result {
     private Integer code;//响应码，1 代表成功; 0 代表失败
     private String msg;  //响应信息 描述字符串
     private Object data; //返回的数据
+
+    public Result(int i, String success, Object o) {
+    }
+
     //增删改 成功响应
     public static Result success(){
         return new Result(0,"success",null);
@@ -23,4 +23,6 @@ public class Result {
     public static Result error(String msg){
         return new Result(1,msg,null);
     }//返回报错信息
+    
+    
 }
