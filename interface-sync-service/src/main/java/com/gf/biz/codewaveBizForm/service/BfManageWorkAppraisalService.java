ba7a9d09cd1ba45d17392d3d7469bbe0242f76f0
kayda <291126509@qq.com>
2024-10-25 17:38:49 +0800

    增加绩效业务持久层代码

---
+++ interface-sync-service/src/main/java/com/gf/biz/codewaveBizForm/service/BfManageWorkAppraisalService.java
@@ -0,0 +1,16 @@
+package com.gf.biz.codewaveBizForm.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.gf.biz.codewaveBizForm.po.BfManageWorkAppraisal;
+
+/**
+ * <p>
+ * 管理人员工作表现鉴定表 服务类
+ * </p>
+ *
+ * @author Gf
+ * @since 2024-10-25 17:35:32
+ */
+public interface BfManageWorkAppraisalService extends IService<BfManageWorkAppraisal> {
+
+}
