1a0f64061e0fdd0d670a3c858c69743099463378
kayda <291126509@qq.com>
2024-10-28 17:27:53 +0800

    增加绩效业务持久层代码

---
+++ interface-sync-service/src/main/java/com/gf/biz/codewaveBizForm/mapper/BdDeptRankProportionMapper.java
@@ -0,0 +1,16 @@
+package com.gf.biz.codewaveBizForm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gf.biz.codewaveBizForm.po.BdDeptRankProportion;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Gf
+ * @since 2024-10-28 17:15:31
+ */
+    public interface BdDeptRankProportionMapper extends BaseMapper<BdDeptRankProportion> {
+
+}
